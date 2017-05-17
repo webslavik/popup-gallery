1beccda8dbdcf86b09a0940f6ab9e92baabaa389
Slavik Sukhanov <slavik.sukhanov@gmail.com>
2017-05-17 22:29:57 +0300

    start

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const gulp 						= require('gulp'),
+			pug 						= require('gulp-pug'),
+			sass            = require('gulp-sass'),
+			autoprefixer    = require('gulp-autoprefixer'),
+			cleanCSS        = require('gulp-clean-css'), // минификация css
+			gcmq 						= require('gulp-group-css-media-queries'), // объединение media queries
+			uglify          = require('gulp-uglify'), // минифицируем js
+			notify          = require("gulp-notify"), // выводим ошибки
+			del             = require('del'),
+			browserSync     = require('browser-sync');
+
+
+
+gulp.task('pug', function() {
+	return gulp.src('src/pug/*.pug')
+		.pipe(pug({
+				pretty: true
+		}))
+		.on('error', notify.onError(function(err) {
+			return {
+				title: 'Jade',
+				message: err.message
+			}
+		}))
+		.pipe(gulp.dest('src/'));
+});
+
+
+gulp.task('sass', function() {
+	return gulp.src('src/sass/*.sass')
+		.pipe(sass())
+		.on('error', notify.onError(function(err) {
+			return {
+				title: 'Sass',
+				message: err.message
+			}
+		}))
+		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+		.pipe(gulp.dest('src/css/'))
+		.pipe(gcmq())
+		.pipe(gulp.dest('src/css/'))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+				baseDir: 'src/'
+		},
+		notify: false
+	});
+});
+
+
+gulp.task('clean', function() {
+	return del.sync('dist');
+});
+
+
+gulp.task('watch', ['browser-sync',  'sass', 'pug'], function() {
+	gulp.watch('src/sass/**/*', ['sass']);
+	gulp.watch('src/pug/**/*.pug', ['pug']);
+	gulp.watch('src/js/**/*', browserSync.reload);
+	gulp.watch('src/*.html', browserSync.reload);
+});
+
+gulp.task('default', ['watch']);
