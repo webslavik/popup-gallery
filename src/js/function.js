85d7ae3d1df6709782162bd47abc0b904da9d88a
Slavik Sukhanov <slavik.sukhanov@gmail.com>
2017-05-18 06:49:29 +0300

    added Next/Previous button

--- src/js/function.js
+++ src/js/function.js
@@ -1,26 +1,57 @@
 'use strict';
 
-(function () {
+(function() {
 
 	let images = document.querySelectorAll('.photo');
 	let overlay = document.querySelector('.overlay');
 
 	images.forEach(function(image, index) {
 		image.addEventListener('click', function(event) {
-			// console.log(event);
-			// console.log(index);
 			showModal(index);
 		});
 	});
 
+
 	function showModal(index) {
 		overlay.classList.toggle('is-show');
+		overlay.stopPropagation();
 	}
 
-	overlay.addEventListener('click', function() {
-		this.classList.toggle('is-show');
+	// overlay.addEventListener('click', function() {
+	// 	this.classList.toggle('is-show');
+	// });
+
+
+	let slides = document.querySelectorAll('.slide');
+	let next = document.querySelector('#next');
+	let previous = document.querySelector('#previous');
+	let controls = document.querySelectorAll('.controls');
+	let currentSlide = 0;
+
+
+	function nextSlide() {
+		goToSlide(currentSlide + 1);
+	}
+
+	function previousSlide() {
+		goToSlide(currentSlide - 1);
+	}
+
+	function goToSlide(n) {
+		slides[currentSlide].classList.remove('is-show');
+		currentSlide = (n + slides.length) % slides.length;
+		slides[currentSlide].classList.add('is-show');
+
+		console.log(currentSlide);
+	}
+
+	next.addEventListener('click', function() {
+		nextSlide();
 	});
 
+	previous.addEventListener('click', function() {
+		previousSlide();
+	});
 
 
 
