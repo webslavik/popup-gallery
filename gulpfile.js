63d8cfc2ade22d7e69e6d84d4ad4f2dbfe376c6e
Slavik Sukhanov <slavik.sukhanov@gmail.com>
2017-05-18 14:57:37 +0300

    added gulp-if and gulp-uglify; created tesk Build

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ const gulp 						= require('gulp'),
 			gcmq 						= require('gulp-group-css-media-queries'), // объединение media queries
 			uglify          = require('gulp-uglify'), // минифицируем js
 			notify          = require("gulp-notify"), // выводим ошибки
+			gulpif 					= require('gulp-if'),
+			useref 					= require('gulp-useref'),
 			del             = require('del'),
 			browserSync     = require('browser-sync');
 
@@ -20,7 +22,7 @@ gulp.task('pug', function() {
 		}))
 		.on('error', notify.onError(function(err) {
 			return {
-				title: 'Jade',
+				title: 'pug',
 				message: err.message
 			}
 		}))
@@ -57,6 +59,15 @@ gulp.task('browser-sync', function() {
 
 gulp.task('clean', function() {
 	return del.sync('dist');
+});
+
+
+gulp.task('build', ['clean', 'sass', 'pug'], function() {
+		var buildHTML = gulp.src('src/*.html')
+			.pipe(useref())
+			// .pipe(gulpif('*.js', uglify()))
+			.pipe(gulpif('*.css', cleanCSS()))
+			.pipe(gulp.dest('dist/'));
 });
 
 
