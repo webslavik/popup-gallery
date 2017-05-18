34e8051bba13aefd85beec3aeb3e536ad86f5ff2
Slavik Sukhanov <slavik.sukhanov@gmail.com>
2017-05-18 05:48:57 +0300

    added Images and Overlay

--- src/js/function.js
+++ src/js/function.js
@@ -2,6 +2,26 @@
 
 (function () {
 
-	console.log('hi');
+	let images = document.querySelectorAll('.photo');
+	let overlay = document.querySelector('.overlay');
+
+	images.forEach(function(image, index) {
+		image.addEventListener('click', function(event) {
+			// console.log(event);
+			// console.log(index);
+			showModal(index);
+		});
+	});
+
+	function showModal(index) {
+		overlay.classList.toggle('is-show');
+	}
+
+	overlay.addEventListener('click', function() {
+		this.classList.toggle('is-show');
+	});
+
+
+
 
 })();
