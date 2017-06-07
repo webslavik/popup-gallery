27aad6e682288b36eeab7caf82c3991e44957402
Slavik <slavik.sukhanov@iskyweb.com>
2017-06-07 16:20:59 +0300

    deleted old code; small fixes

--- src/js/function.js
+++ src/js/function.js
@@ -1,198 +1,13 @@
 'use strict';
 
-// (function() {
-
-// 	let images = document.querySelectorAll('.photo');
-// 	let overlay = document.querySelector('#overlay');
-// 	let slideList = document.querySelector('.slides-list');
-// 	let next = document.querySelector('#next');
-// 	let previous = document.querySelector('#previous');
-// 	let thumbnailsTrack = document.querySelector('.thumbnails-track');
-// 	let thumbnail = document.querySelectorAll('.thumbnail');
-
-// 	let slide;
-// 	let thumbnailsLength;
-// 	let currentSlide = 0;
-// 	let imagesLink = [];
-// 	let count = 0;
-// 	let move = 172;
-
-
-// 	images.forEach(function(el, index) {
-// 		el.addEventListener('click', function(event) {
-// 			currentSlide = index;
-// 			showModal();
-// 			getImagesLink();
-// 			createSlides();
-// 			createThumbnails();
-// 		});
-// 	});
-
-// 	function showModal() {
-// 		overlay.classList.add('is-show');
-// 	}
-
-
-// 	function createSlides() {
-// 		for (let i = 0; i < imagesLink.length; i++) {
-// 			let li = document.createElement('li');
-// 			let div = document.createElement('div');
-
-// 			if (i == currentSlide)
-// 				li.classList.add('slide', 'is-show');
-// 			else		
-// 				li.classList.add('slide');
-
-// 			div.classList.add('slide-image');
-// 			div.style.backgroundImage = imagesLink[i];
-
-// 			li.appendChild(div);
-// 			slideList.appendChild(li);
-// 		}
-
-// 		slide = document.querySelectorAll('.slide');
-// 	}
-
-// 	function createThumbnails() {
-// 		for (let i = 0; i < imagesLink.length; i++) {
-// 			let li = document.createElement('li');
-// 			let div = document.createElement('div');
-
-// 			if (i == currentSlide)
-// 				li.classList.add('thumbnail', 'is-active');
-// 			else		
-// 				li.classList.add('thumbnail');
-
-// 			div.classList.add('thumbnail-photo');
-// 			div.style.backgroundImage = imagesLink[i];
-
-// 			li.appendChild(div);
-// 			thumbnailsTrack.appendChild(li);
-// 		}
-
-// 		thumbnail = document.querySelectorAll('.thumbnail');
-// 		// console.log(thumbnail);
-// 		thumbnailClick();
-// 	}
-
-// 	function getImagesLink() {
-// 		images.forEach(function(el) {
-// 			let children = el.childNodes;
-// 			for (let i = 0; i < children.length; i++) {
-// 				if (children[i].tagName == 'DIV')
-// 					imagesLink.push(children[i].style.backgroundImage);
-// 			}
-// 		});
-// 	}
-
-
-// 	overlay.addEventListener('click', function(evet) {
-// 		if (!evet.target.classList.contains('overlay'))
-// 			return false;
-
-// 		this.classList.remove('is-show');
-// 		imagesLink = [];
-// 		removeSlideChildren(slideList);
-// 		removeSlideChildren(thumbnailsTrack);
-// 	});
-
-// 	function removeSlideChildren(element) {
-// 		while (element.firstChild)
-// 			element.removeChild(element.firstChild);
-// 	}
-
-
-// 	/*
-// 		------------------------------------------------
-// 			Navigation
-// 		------------------------------------------------
-// 	*/ 
-// 	next.addEventListener('click', function() {
-// 		nextPhoto();
-// 	});
-
-// 	previous.addEventListener('click', function() {
-// 		previousPhoto();
-// 	});
-	
-// 	function nextPhoto() {
-// 		goToPhoto(currentSlide + 1);
-// 		thumbnailsTrackMove('next');
-// 	}
-
-// 	function previousPhoto() {
-// 		goToPhoto(currentSlide - 1);
-// 		thumbnailsTrackMove('prev');
-// 	}
-
-
-// 	function thumbnailClick() {
-// 		thumbnailsLength = thumbnail.length;
-// 		thumbnail.forEach(function(el, index) {
-// 			el.addEventListener('click', function() {
-// 				goToPhoto(index);
-// 			});
-// 		})
-// 	}
-
-// 	function goToPhoto(index) {
-// 		slide[currentSlide].classList.remove('is-show');
-// 		thumbnail[currentSlide].classList.remove('is-active');
-// 		currentSlide = (index + slide.length) % slide.length;
-// 		slide[currentSlide].classList.add('is-show');
-// 		thumbnail[currentSlide].classList.add('is-active');
-// 	}
-
-// 	function thumbnailsTrackMove(direction) {
-		
-// 		if (direction == 'next') {
-// 			count = (count + 1) % thumbnailsLength;
-// 			let moveDistance = count * move;
-// 			console.log(count);
-// 			if (moveDistance >= 516) {
-// 				thumbnailsTrack.style.transform = 'translateX(-' + 516 + 'px)';
-// 			} else if (count === (thumbnail.length - 1))  {
-// 				thumbnailsTrack.style.transform = 'translateX(' + 0 + 'px)';
-// 			} else {
-// 				thumbnailsTrack.style.transform = 'translateX(-' + (count * move) + 'px)';
-// 			}
-// 		} 
-
-// 		if (direction == 'prev') {
-// 			count = (count - 1) % thumbnailsLength;
-// 			let moveDistance = count * move;
-
-// 			if (count === -1) {
-// 				count = thumbnailsLength - 1;
-// 				thumbnailsTrack.style.transform = 'translateX(-' + 516 + 'px)';
-// 			} else {
-// 				thumbnailsTrack.style.transform = 'translateX(-' + count * (move / 2) + 'px)';
-// 			}
-// 		}
-
-// 		// if (direction == 'next') {
-// 		// 	thumbnailsTrack.style.transform = 'translateX(-' + (count * move) + 'px)';
-// 		// 	count = (count + 1) % thumbnailsLength;
-// 		// } else {
-// 		// 	count = (count - 1) % thumbnailsLength;
-// 		// 	if ( count == -1 )
-// 		// 		count = thumbnailsLength - 1;
-// 		// 	// thumbnailsTrack.style.transform = 'translateX(' + (count * move) + 'px)'
-// 		// }
-// 	}
-
-// })();
-
-
-
 (function() {
 
-	function SkySlider(element) {
-		this.init(element);
+	function SkySlider(items) {
+		this.init(items);
 	}
 
-	SkySlider.prototype.init = function(element) {
-		this.el = document.querySelector(element);
+	SkySlider.prototype.init = function(items) {
+		this.el = document.querySelector('.skySlider');
 		this.images = this.el.querySelectorAll('img');
 
 		this.overlay = null;
@@ -211,8 +26,10 @@
 		this.index = 0;
 		this.total = 0;
 
+		this.test = 'Jack';
+
 		this.defatulSettings = {
-			thumbnailItems: 5,
+			thumbnailItems: items || 5,
 		}
 	};
 
@@ -286,7 +103,6 @@
 		//--------------------------------------------------------
 		this.thumbnailsWrap = document.createElement('div');
 		this.thumbnailsWrap.classList.add('skySlider-thumbnails-wrap');
-		// this.thumbnailsWrap.style.width = (this.thumbnailsBelt.children[0].clientWidth;
 
 		this.thumbnailsBelt = document.createElement('ul');
 		this.thumbnailsBelt.classList.add('skySlider-thumbnails-belt');
@@ -303,8 +119,6 @@
 			this.thumbnailsBelt.appendChild(this.thumbnail);
 		}
 		this.thumbnailsWrap.appendChild(this.thumbnailsBelt);
-		// this.thumbnailsWrap.style.width = this.thumbnailWrapperWidth();
-
 		this.overlay.appendChild(this.thumbnailsWrap);
 
 		document.body.appendChild(docFrag);
@@ -357,6 +171,23 @@
 		});
 	}
 
+	SkySlider.prototype.moveBelt = function(direction) {
+		if (direction == 'right') {
+			let moveWidth = 131 * (this.index);
+			this.thumbnailsBelt.style.left = `-${moveWidth}px`;
+
+			console.log(moveWidth);
+		}
+
+		if (direction == 'left') {
+			let moveWidth = 131 * (this.index);
+			this.thumbnailsBelt.style.left = `${moveWidth}px`;
+
+			console.log(moveWidth);
+		}
+
+	}
+
 	SkySlider.prototype.arrowActions = function(index) {
 		this.prev.addEventListener('click', () => {
 			this.index--;
@@ -367,6 +198,7 @@
 
 			this.thumbnailActive();
 			this.slideTo(this.index);
+			this.moveBelt('left');
 		});
 
 		this.next.addEventListener('click', () => {
@@ -378,6 +210,7 @@
 
 			this.thumbnailActive();
 			this.slideTo(this.index);
+			this.moveBelt('right');
 		});
 	}
 
@@ -397,7 +230,7 @@
 
 
 	//-------------------------------------------------
-	let modal = new SkySlider('.skySlider');
+	let modal = new SkySlider();
 	let images = document.querySelectorAll('.photo');
 
 	images.forEach(function(el, index) {
