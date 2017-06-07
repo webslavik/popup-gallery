2b2448f3f503bcb6d7552ca5c4c4ada99a186703
Slavik <slavik.sukhanov@iskyweb.com>
2017-06-07 14:33:15 +0300

    renamed thumbnailList to thumbnailBelt; set width for  thumbailWrap and thumbnailBelt

--- src/js/function.js
+++ src/js/function.js
@@ -205,19 +205,24 @@
 		this.prev = null;
 
 		this.thumbnailsWrap = null;
-		this.thumbnailsList = null;
+		this.thumbnailsBelt = null;
 		this.thumbnail = null;
 
 		this.index = 0;
 		this.total = 0;
+
+		this.defatulSettings = {
+			thumbnailItems: 5,
+		}
 	};
 
 	SkySlider.prototype.open = function(index) {
-
 		this.index = index;
 		this.create();
-		this.actions(this.index);
+		this.arrowActions(this.index);
 		this.thumbnailsActions();
+		this.thumbnailWrapperWidth();
+
 		this.overlay.classList.add('is-open');
 
 		this.overlay.addEventListener('click', (e) => {
@@ -281,8 +286,10 @@
 		//--------------------------------------------------------
 		this.thumbnailsWrap = document.createElement('div');
 		this.thumbnailsWrap.classList.add('skySlider-thumbnails-wrap');
-		this.thumbnailsList = document.createElement('ul');
-		this.thumbnailsList.classList.add('skySlider-thumbnails-list');
+		// this.thumbnailsWrap.style.width = (this.thumbnailsBelt.children[0].clientWidth;
+
+		this.thumbnailsBelt = document.createElement('ul');
+		this.thumbnailsBelt.classList.add('skySlider-thumbnails-belt');
 
 		for (let i = 0; i < this.images.length; i++) {
 			this.thumbnail = document.createElement('li');
@@ -293,16 +300,18 @@
 			this.img = document.createElement('img');
 			this.img.setAttribute('src', this.images[i].getAttribute('src'));
 			this.thumbnail.appendChild(this.img);
-			this.thumbnailsList.appendChild(this.thumbnail);
+			this.thumbnailsBelt.appendChild(this.thumbnail);
 		}
-		this.thumbnailsWrap.appendChild(this.thumbnailsList);
+		this.thumbnailsWrap.appendChild(this.thumbnailsBelt);
+		// this.thumbnailsWrap.style.width = this.thumbnailWrapperWidth();
+
 		this.overlay.appendChild(this.thumbnailsWrap);
 
 		document.body.appendChild(docFrag);
 	}
 
 	SkySlider.prototype.thumbnailsActions = function() {
-		let thumbnails = this.thumbnailsList.children;
+		let thumbnails = this.thumbnailsBelt.children;
 
 		Array.from(thumbnails, (el, index) => {
 			el.addEventListener('click', () => {
@@ -319,8 +328,36 @@
 			});
 		});
 	}
-	
-	SkySlider.prototype.actions = function(index) {
+
+	SkySlider.prototype.thumbnailWrapperWidth = function() {
+		let style = window.getComputedStyle(this.thumbnailsBelt.children[0]),
+			width = this.thumbnailsBelt.children[0].offsetWidth,
+			margin = parseFloat(style.marginLeft) + parseFloat(style.marginRight),
+			padding = parseFloat(style.paddingLeft) + parseFloat(style.paddingRight),
+			border = parseFloat(style.borderLeftWidth) + parseFloat(style.borderRightWidth);
+
+		let sum = width + margin + padding + border;
+		let wrapWidth = sum * this.defatulSettings.thumbnailItems;
+		let listWidth = sum * this.total;
+
+		this.thumbnailsWrap.style.width = `${wrapWidth}px`;
+		this.thumbnailsBelt.style.width = `${listWidth}px`;
+	}
+
+	SkySlider.prototype.thumbnailActive = function() {
+		let thumbnails = this.thumbnailsBelt.children;
+		Array.from(thumbnails, (el, index) => {
+			let siblings = el.parentNode.children;
+
+			for (let i = 0; i < siblings.length; i++) 
+				if (siblings[i].classList.contains('is-current'))
+					siblings[i].classList.remove('is-current');
+			
+				siblings[this.index].classList.add('is-current');
+		});
+	}
+
+	SkySlider.prototype.arrowActions = function(index) {
 		this.prev.addEventListener('click', () => {
 			this.index--;
 
@@ -341,19 +378,6 @@
 
 			this.thumbnailActive();
 			this.slideTo(this.index);
-		});
-	}
-
-	SkySlider.prototype.thumbnailActive = function() {
-		let thumbnails = this.thumbnailsList.children;
-		Array.from(thumbnails, (el, index) => {
-			let siblings = el.parentNode.children;
-
-			for (let i = 0; i < siblings.length; i++) 
-				if (siblings[i].classList.contains('is-current'))
-					siblings[i].classList.remove('is-current');
-			
-				siblings[this.index].classList.add('is-current');
 		});
 	}
 
